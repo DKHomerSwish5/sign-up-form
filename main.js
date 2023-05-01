23f483ca8f70d99caf6a6c5de91a25be10fe9ea2
Joe <csp.x520@gmail.com>
2023-04-30 21:25:01 -0400

    Added DOM selectors for user inputs

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-let input = document.querySelectorAll('[id$="container"]');
+let firstName = document.querySelector('[id^="firstname"]');
+let lastName = document.querySelector('[id^="lastname"]');
+let email = document.querySelector('[id^="email"]');
+let phoneNumber = document.querySelector('[id^="phone-number"]');
+let password = document.querySelector('[id^="password"]');
+let cPassword = document.querySelector('[id^="confirm"]');
 let myBtn = document.querySelector('[button$="button"]');
+let firstNameInput;
+myBtn.addEventListener('click',buttonClick);
+function buttonClick(){
+    
+}
+firstName.addEventListener('input',updateFirstName);
+function updateFirstName(){
+    firstNameInput = firstName.value;
+}
