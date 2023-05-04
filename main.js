a87ab96c7ba70968150b4e1b622b8295a4584acc
Joe <csp.x520@gmail.com>
2023-05-03 20:52:26 -0400

    Have email validation check

--- main.js
+++ main.js
@@ -6,11 +6,26 @@ let password = document.querySelector('[id^="password"]');
 let cPassword = document.querySelector('[id^="confirm"]');
 let myBtn = document.querySelector('[button$="button"]');
 let firstNameInput;
+let lastNameInput;
 myBtn.addEventListener('click',buttonClick);
 function buttonClick(){
     
 }
 firstName.addEventListener('input',updateFirstName);
+email.addEventListener('input',validEmailEntry)
 function updateFirstName(){
     firstNameInput = firstName.value;
+}
+function updateLastName(){
+    lastNameInput = lastName.value;
+}
+function validEmailEntry(emailInput){
+    let emailCheck = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+    if(emailCheck.test(emailInput)){
+        return true;
+    }
+    else{
+        return "Please enter a valid email address";
+    }
 }
