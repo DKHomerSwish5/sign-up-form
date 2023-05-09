b7d521b66cbfbab1ce7677f431ff238920d43d7f
Joe <csp.x520@gmail.com>
2023-05-08 20:45:28 -0400

    Have the submit button work with form inputs

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ let email = document.getElementById("user-email");
 let phoneNumber = document.getElementById("phone-number");
 let password = document.getElementById("user-password");
 let cPassword = document.getElementById("confirm-user-pword");
-let myBtn = document.querySelector(".create-account-button");
-let form = document.querySelector(".sign-up-container");
 let firstNameInput;
 let lastNameInput;
 let numberInput;
@@ -13,22 +11,13 @@ let emailInput;
 let passwordInput;
 let cPasswordInput;
 
-
 firstName.addEventListener('input',updateFirstName);
 lastName.addEventListener('input',updateLastName);
 email.addEventListener('input',updateEmail);
 phoneNumber.addEventListener('input',updateNumber);
 password.addEventListener('input',passwordUpdate);
 cPassword.addEventListener('input',cPasswordUpdate);
-myBtn.addEventListener('click',submitForm);
 
-function submitForm(){
-    cPasswordUpdate();
-    if(cPassword.validationMessage=="Passwords do not match"){
-        return
-    }
-    form.submit();
-}
 function updateFirstName(){
     firstNameInput = firstName.value;
 }
@@ -40,7 +29,6 @@ function updateEmail(){
 }
 function updateNumber(){
     numberInput = phoneNumber.value;
-    numValidity();
 }
 function passwordUpdate(){
     passwordInput = password.value;
